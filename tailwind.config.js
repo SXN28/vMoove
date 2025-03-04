2eb6b9d87e0a9dd2c8d779655f1f6af25e1eef8b
FORAY CLEMENT p2200744 <clement.foray@etu.univ-lyon1.fr>
2025-03-04 18:35:34 +0100

    continuation homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,31 @@ module.exports = {
     extend: {
       colors: {
         // Ajoutez ou modifiez des couleurs ici
-        primary: '#333333', // Rouge
+        fond: '#333333', // Rouge
         secondary: '#00FF00', // Vert
-        accent: '#0000FF', // Bleu
+        bleu: '#0968E7', // Bleu
       },
+
+      fontFamily: {
+        'bridgend': ['bridgend', 'sans-serif'], // Assurez-vous que c'est exactement comme ceci
+      },
+
+      
+      fontSize: {
+        '6xl': '4rem',
+        '7xl': '4.5rem',
+        '8xl': '5rem',
+        '9xl': '6rem',
+        '10xl': '7rem',
+        '11xl': '8rem',
+        '12xl': '9rem',
+      }
     },
   },
   plugins: [
     require('flowbite/plugin')
   ],
 }
+
+
+//symfony console tailwind:build --watch
